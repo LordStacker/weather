5c21b67d9bed0b910bf77901c592c70ca027cce5
Lordstacker <nicoenrique_1994@hotmail.com>
2024-03-11 21:31:00 +0100

    initial commit merged with rasmus

---
+++ lib/weather_app.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import 'weekly_forecast_screen.dart';
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      scrollBehavior: const ConstantScrollBehavior(),
+      title: 'Horizons Weather',
+      home: const WeeklyForecastScreen(),
+    );
+  }
+}
+
+class ConstantScrollBehavior extends ScrollBehavior {
+  const ConstantScrollBehavior();
+
+  @override
+  Widget buildScrollbar(
+          BuildContext context, Widget child, ScrollableDetails details) =>
+      child;
+
+  @override
+  Widget buildOverscrollIndicator(
+          BuildContext context, Widget child, ScrollableDetails details) =>
+      child;
+
+  @override
+  TargetPlatform getPlatform(BuildContext context) => TargetPlatform.macOS;
+
+  @override
+  ScrollPhysics getScrollPhysics(BuildContext context) =>
+      const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics());
+}
